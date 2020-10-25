2c347039d03bf4a70e178879442a741b337bbad1
kellynvd <kellynvd@gmail.com>
2020-10-25 18:39:47 -0300

    feat: Search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,5 +1,70 @@
-export default function Search() {
+import { useRouter } from 'next/router';
+import { FormEvent, useState } from 'react';
+import { Document } from 'prismic-javascript/types/documents';
+import Prismic from 'prismic-javascript';
+import PrismicDom from 'prismic-dom';
+import { GetServerSideProps } from 'next';
+import { client } from '@/lib/prismic';
+import Link from 'next/link';
+
+interface SearchProps {
+  searchResults: Document[];
+}
+
+export default function Search({ searchResults }: SearchProps) {
+  const [search, setSearch] = useState('');
+  const router = useRouter()
+
+  function handleSearch (e: FormEvent) {
+    e.preventDefault();
+
+    router.push(
+      `/search?q=${encodeURIComponent(search)}`
+    )
+
+    setSearch('');
+  }
+
   return (
-    <h1>Search</h1>
+    <div>
+      <form onSubmit={handleSearch}>
+        <input type='text' value={search} onChange={ e => setSearch(e.target.value)} />
+        <button type='submit'>Search</button>
+      </form>
+
+
+      <ul>
+      {searchResults.map(searchResult => {
+        return (
+          <li key={searchResult.id}>
+            <Link href={`catalog/products/${searchResult.uid}`}>
+                <a>
+                  {PrismicDom.RichText.asText(searchResult.data.title)}
+                </a>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<SearchProps> = async (context) => {
+  const { q } = context.query;
+
+  if (!q) {
+    return { props: { searchResults: [] }};
+  }
+
+  const searchResults = await client().query([
+    Prismic.Predicates.at('document.type', 'product'),
+    Prismic.Predicates.fulltext('my.product.title', String(q))
+  ])
+
+  return {
+    props: {
+      searchResults: searchResults.results,
+    }
+  }
 }
